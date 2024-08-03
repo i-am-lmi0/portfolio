39b732e50ce633ece79a8e618e2a545807e1401f
i-am-lmi0 <arnav.somaghatta@icloud.com>
2024-08-03 14:01:29 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-
+const express = require('express');
+const app = express();
+const server = app.listen(8080, () => {
+    console.log(`Server running on port ${server.address().port}`);
+});
+app.use(express.static('public'))
+console.log("imported pages")
+app.use(express.static('img')) 
+console.log("imported img")
+console.log("static imports finished")
