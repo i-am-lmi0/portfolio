1fff4878cff5e7c96554496ae69ac58c1288df7d
i-am-lmi0 <arnav.somaghatta@icloud.com>
2024-08-03 20:14:27 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
-const express = require('express');
-const app = express();
-const server = app.listen(8080, () => {
-    console.log(`Server running on port ${server.address().port}`);
-});
-app.use(express.static('public'))
-console.log("imported pages")
-app.use(express.static('img')) 
-console.log("imported img")
-console.log("static imports finished")
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
