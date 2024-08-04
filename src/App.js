e2b7895b7224c94b07676618fa1a3d3a9168dbcf
i-am-lmi0 <arnav.somaghatta@icloud.com>
2024-08-03 20:51:24 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,1 +1,11 @@
+import React from 'react';
 
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to Arnav's Coding Portfolio</h1>
+    </div>
+  );
+}
+
+export default App;
